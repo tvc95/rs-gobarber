3a62d6c8ba3e8314ae5862fb88cdab4af8188578
tvc95 <thiagovc1500@gmail.com>
2021-04-21 13:25:00 -0300

    Corrected Forgot Password page

--- src/Pages/Forgot Password/index.tsx
+++ src/Pages/Forgot Password/index.tsx
@@ -2,14 +2,15 @@ import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiUser } from 'react-icons/fi';
-import { useHistory } from 'react-router';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import * as Yup from 'yup';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { useToast } from '../../hooks/ToastContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { Background, Container, Content } from './styles';
+
+import logoImg from '../../assets/svg/logo.svg';
 
 interface ForgotPasswordFormData {
   email: string;
